ec76a126a42097407ebd919f41950efb77a7390a
Abhijeet Ashok Muneshwar <openingknots@gmail.com>
2017-03-27 23:41:34 +0530

    Protect Singleton from serialization and reflection attacks

---
+++ src/com/designpatterns/creationalpatterns/singletonpattern/AdvanceSingleton.java
@@ -0,0 +1,40 @@
+package com.designpatterns.creationalpatterns.singletonpattern;
+
+/**
+ * Created using IntelliJ IDEA.
+ * User: abhijeet
+ * Date: 27/03/17
+ * Time: 11:13 PM
+ */
+public class AdvanceSingleton {
+    private static AdvanceSingleton instance = null;
+
+    /**
+     * To avoid creation of new instance on reflection
+     * @throws IllegalStateException
+     */
+    private AdvanceSingleton() throws IllegalStateException {
+        if (instance != null) {
+            throw new IllegalStateException("Instance is already available.");
+        }
+    }
+
+    /**
+     * To avoid creation of new instance on serialization
+     * @return
+     */
+    protected Object readResolve() {
+        return getInstance();
+    }
+
+    public static AdvanceSingleton getInstance() {
+        if (instance == null) {
+            synchronized (AdvanceSingleton.class) {
+                if (instance == null) {
+                    instance = new AdvanceSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
